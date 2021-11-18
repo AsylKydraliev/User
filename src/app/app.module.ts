aa152a812b00953abf4cda60160351cea83f86b8
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2021-11-18 20:53:26 +0600

    Созданы 2 компонента new-user и users для создания пользовалей

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
